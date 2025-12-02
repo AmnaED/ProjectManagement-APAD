845dcd054e60cfd14fa61f5028d5c5ae1976d068
AmnaED <amna.aldarmakiii@gmail.com>
2025-12-02 09:44:57 -0600

    adjusted navigation for project managment page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ProjectManagementPage from './ProjectManagementPage';
-
-
-
 
 function App() {
   return (
@@ -11,6 +8,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/project-management/:project_id" element={<ProjectManagementPage />} />
+          <Route path="/" element={<Navigate to="/project-management/204" replace />} />
+          <Route path="/project-management" element={<Navigate to="/project-management/204" replace />}/>
         </Routes>
       </Router>
     </div>
