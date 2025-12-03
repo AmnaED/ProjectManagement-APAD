0ef0449483fd0aa279221ed78d7a3bd447d65cd2
isabellaesquivel <esquivelisabellaa@gmail.com>
2025-12-02 18:48:27 -0600

    Presentation Code

--- client/src/config.js
+++ client/src/config.js
@@ -2,9 +2,9 @@ let API_BASE_URL = process.env.REACT_APP_API_URL;
 
 // Optional override based on current hostname
 if (window.location.hostname === "127.0.0.1") {
-  API_BASE_URL = "http://127.0.0.1:5000";
+  API_BASE_URL = "http://127.0.0.1:5003";
 } else if (window.location.hostname === "localhost") {
-  API_BASE_URL = "http://localhost:5001";
+  API_BASE_URL = "http://localhost:5003";
 }
 
 export default API_BASE_URL;
